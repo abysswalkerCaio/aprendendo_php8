7c1dc5db1422b53cc86c3dfc9006d6bbd79a30da
Caio Costa <hcaio370@gmail.com>
2022-07-13 11:18:50 -0300

    Update strings.php

--- strings.php
+++ strings.php
@@ -1,11 +1,17 @@
 <?php
 
 $texto = "Texto já";
-$texto .= " concatenado diretamente.";
+$texto .= " concatenado diretamente. ";
 
 $interpretacao = "$texto"; //Com o uso de aspas duplas, a variável é interpretada e o seu valor é utilizado.
 $interpretacao = '$texto'; //Com o suo de aspas simples, a variável não é interpretada.
 
 echo $texto;
+
+$descricao = <<<LABEL
+Utilização da LABEL em PHP para a digitação de grandes descrições ou textos grandes.
+LABEL;
+
+echo $descricao;
 
 ?>
