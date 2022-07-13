901553f7696bd6d2eab4483a8f7f14190270b39a
Caio Costa <hcaio370@gmail.com>
2022-07-13 12:45:10 -0300

    Create php_html.php

---
+++ php_html.php
@@ -0,0 +1,21 @@
+<?php
+
+$nome = "Caio";
+$apelido = "ciao";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nome & Apelido</title>
+</head>
+<body>
+    <h3>O meu nome é: <?=$nome?></h3>
+    <br>
+    <h3>O meu apelido é: <?=$apelido?></h3>
+</body>
+</html>
