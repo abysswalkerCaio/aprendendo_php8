c2ae3ee6f386dae26bd01a09a182ba0263696365
Caio Costa <hcaio370@gmail.com>
2022-07-13 11:10:22 -0300

    Create introducao.php

---
+++ introducao.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "O conteúdo de echo pode ser inserido em aspas duplas.";
+echo '<br><br>';
+echo 'O conteúdo de echo pode ser inserido em aspas simples.';
+echo '<br><br>';
+echo ("O conteúdo de echo pode ser inserido em aspas duplas dentro de parênteses.");
+echo '<br><br>';
+echo ('O conteúdo de echo pode ser inserido em aspas simples dentro de parênteses.');
+echo '<br>';
+echo "<h3>É possível utilizar tags HTML dentro de echo com PHP.</h3>"
+
+//Comentário em uma linha com PHP.
+
+/*
+Comentário em 
+diversas linhas 
+com PHP.
+*/
+
+?>
