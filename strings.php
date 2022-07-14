b52e114a913c0b8ea194fae4587462b341ac8b6b
Caio Costa <hcaio370@gmail.com>
2022-07-14 09:34:55 -0300

    Update strings.php

--- strings.php
+++ strings.php
@@ -1,7 +1,7 @@
 <?php
 
-$texto = "Texto já";
-$texto .= " concatenado diretamente. ";
+$texto = 'Texto já';
+$texto .= ' concatenado diretamente. ';
 
 $interpretacao = "$texto"; //Com o uso de aspas duplas, a variável é interpretada e o seu valor é utilizado.
 $interpretacao = '$texto'; //Com o suo de aspas simples, a variável não é interpretada.
