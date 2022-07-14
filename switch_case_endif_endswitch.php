83a31d1750d9fea2ad9b07efa3346a5e54564955
Caio Costa <hcaio370@gmail.com>
2022-07-14 09:34:04 -0300

    Update switch_case_endif_endswitch.php

--- switch_case_endif_endswitch.php
+++ switch_case_endif_endswitch.php
@@ -1,38 +1,38 @@
 <?php
 
-$nome = "Caio";
+$nome = 'Caio';
 
 switch($nome) {
-    case "Caio":
-        echo "O nome é Caio!";
+    case 'Caio':
+        echo 'O nome é Caio!';
         break;
-    case "Ryan":
-        echo "O nome é Ryan!";
+    case 'Ryan':
+        echo 'O nome é Ryan!';
         break;
     default:
-        echo "O nome não é Caio nem Ryan!";
+        echo 'O nome não é Caio nem Ryan!';
         break;
 }
 
 # Podemos criar uma estrutura condicional alternativa com endif.
 
-if ($nome == "Caio"):
-    echo "<br>O nome é Caio!<br>";
+if ($nome == 'Caio'):
+    echo '<br>O nome é Caio!<br>';
 else:
-    echo "<br>O nome não é Caio!<br>";
+    echo '<br>O nome não é Caio!<br>';
 endif;
 
 # Podemos também criar uma estrutura condicional alternativa com endswitch.
 
 switch($nome):
-    case "Caio":
-        echo "Estrutura alternativa com switch e o nome é Caio!";
+    case 'Caio':
+        echo 'Estrutura alternativa com switch e o nome é Caio!';
         break;
-    case "Ryan":
-        echo "Estrutura alternativa com switch e o nome é Ryan!";
+    case 'Ryan':
+        echo 'Estrutura alternativa com switch e o nome é Ryan!';
         break;
     default:
-        echo "Estrutura alternativa com switch e o nome não é Caio nem Ryan!";
+        echo 'Estrutura alternativa com switch e o nome não é Caio nem Ryan!';
         break;
 endswitch;
 
