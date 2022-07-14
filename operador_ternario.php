e3050582743c7778fc9a8d1779972e2a943e456f
Caio Costa <hcaio370@gmail.com>
2022-07-14 09:27:39 -0300

    Create operador_ternario.php

---
+++ operador_ternario.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = 'Caio';
+$apelido = 'ciao';
+
+echo $nome == 'Caio' ? 'Sim, com certeza.<br>' : 'Não, claro que não.<br>';
+echo $apelido == 'GK' ? 'Sim, com certeza.' : 'Não, claro que não.';
+
+?>
